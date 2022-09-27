2034c116b6d291a65dffc92fb2d2b62537bc2314
kimbank <kimeunhang@inha.edu>
2022-09-28 03:56:25 +0900

    update: iframe api

---
+++ js/test.js
@@ -0,0 +1,20 @@
+const DEBUG_MODE = 1;
+
+console.log(
+    "-*-*-*-*-*-*-*-*-*-*-*-\n"
+    + "| debug mode " + (DEBUG_MODE ? "Enabled" : "Disabled")
+    + " |\n-----------------------"
+    );
+
+var AFFCTIV_AI = "    ___    ________     __  _          ___    ____\n\
+   /   |  / __/ __/____/ /_(_)   __   /   |  /  _/\n\
+  / /| | / /_/ /_/ ___/ __/ / | / /  / /| |  / /  \n\
+ / ___ |/ __/ __/ /__/ /_/ /| |/ /  / ___ |_/ /   \n\
+/_/  |_/_/ /_/  \\___/\\__/_/ |___/  /_/  |_/___/   \n\
+"
+
+var LAB_URL = "https://affctiv.ai/"
+
+console.log(AFFCTIV_AI)
+console.log("Check Out -> " + LAB_URL)
+console.log("2022. Laboratory Intern. Team A(In)")
